edff9deb3664cada9327161104d735310ada8864
Onen Felix <onenfelix90@gmail.com>
2023-08-28 05:43:28 +0300

    deep dive into arrays, strings and stringBuffers

--- Method.java
+++ Method.java
@@ -1,22 +1,35 @@
 class Computer {
-    public void playMusic(){
+    public void playMusic() {
         System.out.println("Playing Music Now....");
     }
 
-    public String getMeApen(int cost){
-        if(cost >= 10)
+    public String getMeApen(int cost) {
+        if (cost >= 10)
             return "Pen";
         return "Nothing";
     }
 }
 
 public class Method {
-    public static void main(String a[]){
+    public static void main(String a[]) {
 
-        Computer obj = new Computer();
-        obj.playMusic();
-        String pen = obj.getMeApen(12);
-        System.out.println(pen);
+        int nums[][] = new int[3][];
+        nums[0] = new int[3];
+        nums[1] = new int[4];
+        nums[2] = new int[2];
 
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                nums[i][j] = (int) (Math.random() * 100);
+            }
+
+        }
+
+        for (int n[] : nums) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
     }
 }
